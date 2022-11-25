ae8acde61db3f40679296c2d535d339726d47f7e
Shubham <shubham21.skl@gmail.com>
2022-11-25 11:19:26 +0530

    AppDelegate.swift class

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,56 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+        GeneratedPluginRegistrant.register(with: self)
+        FlutterDownloaderPlugin.setPluginRegistrantCallback(registerPlugins)
+        //CleverTap.autoIntegrate() // integrate CleverTap SDK using the autoIntegrate option
+        //CleverTapPlugin.sharedInstance()?.applicationDidLaunch(options: launchOptions)
+        if #available(iOS 10.0, *) {
+            UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+        }
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
+    // Receive APNS token
+    override func application(_ application: UIApplication,
+                              didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        CleverTap.sharedInstance()?.setPushToken(deviceToken)
+        // 1. Convert device token to string
+        let tokenParts = deviceToken.map { data -> String in
+            return String(format: "%02.2hhx", data)
+        }
+        let token = tokenParts.joined()
+        // 2. Print device token to use for PNs payloads
+        print("APNS Token: \(token)")
+        super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+    }
+    
+    // When notification comes to the application
+    override public func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                                didReceive response: UNNotificationResponse,
+                                                withCompletionHandler completionHandler: @escaping () -> Void){
+        CleverTap.sharedInstance()?.handleNotification(withData: response.notification.request.content.userInfo)
+        completionHandler()
+    }
+    
+    
+    // When user app status is FOREGROUND
+    override func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler:
+                                @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+        CleverTap.sharedInstance()?.handleNotification(withData: notification.request.content.userInfo, openDeepLinksInForeground: false)
+        
+        // Don't alert the user for other types.
+        completionHandler([.alert, .badge, .sound])
+    }
+
+}
+
+private func registerPlugins(registry: FlutterPluginRegistry) {
+    if (!registry.hasPlugin("FlutterDownloaderPlugin")) {
+        FlutterDownloaderPlugin.register(with: registry.registrar(forPlugin: "FlutterDownloaderPlugin")!)
+    }
+
 }
