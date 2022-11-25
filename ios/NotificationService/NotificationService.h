c6dbcb871a3c9927d83257a178b5eef49434712c
Shubham <shubham21.skl@gmail.com>
2022-11-25 11:15:04 +0530

    Added Native  notification Service for ios

---
+++ ios/NotificationService/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  NotificationService
+//
+//  Created by Admin on 17/11/21.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
---
+++ ios/NotificationService/NotificationService.swift
@@ -0,0 +1,37 @@
+//
+//  NotificationService.swift
+//  NotificationService
+//
+//  Created by Admin on 16/11/21.
+//
+
+import UserNotifications
+import FirebaseMessaging
+
+class NotificationService: UNNotificationServiceExtension {
+
+    var contentHandler: ((UNNotificationContent) -> Void)?
+    var bestAttemptContent: UNMutableNotificationContent?
+
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        self.contentHandler = contentHandler
+        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+        
+        if let bestAttemptContent = bestAttemptContent {
+            // Modify the notification content here...
+            FirebaseMessaging.FIRMessagingExtensionHelper().populateNotificationContent(bestAttemptContent, withContentHandler: contentHandler)
+            // This code was older
+//            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
+//            contentHandler(bestAttemptContent)
+        }
+    }
+    
+    override func serviceExtensionTimeWillExpire() {
+        // Called just before the extension will be terminated by the system.
+        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+            contentHandler(bestAttemptContent)
+        }
+    }
+
+}
